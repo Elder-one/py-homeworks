8d885d04f29bbd9ed105f62600b3b9c7fb7fd8a0
Alex <aleksan672@gmail.com>
2023-03-26 17:35:26 +0300

    Added homework04

---
+++ homework04/task1.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def QSort(array, start, stop):
+    if start >= stop:
+        return
+    pivot = get_pivot(array, start, stop)
+    QSort(array, start, pivot-1)
+    QSort(array, pivot, stop)
+
+
+def get_pivot(array, start, stop):
+    pivot = array[stop]
+    curr_pos = start
+    for i in range(start, stop+1):
+        if array[i] <= pivot:
+            array[i], array[curr_pos] = array[curr_pos], array[i]
+            curr_pos += 1
+    if curr_pos > stop:
+        curr_pos -= 1
+    return curr_pos
+
+
+arr = [randint(1, 10000) for _ in range(1000)]
+QSort(arr, 0, len(arr)-1)
+print(arr)
+
